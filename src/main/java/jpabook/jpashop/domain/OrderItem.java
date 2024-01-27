a9ebcd5cb18d01039aff97a0846e29c230e325c2
yerimya <syl1602@naver.com>
2024-01-27 17:51:27 +0900

    feat: 주문 엔티티, 리포지토리, 서비스 개발 및 주문 기능 테스트

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -25,4 +28,32 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
 
     private int count; // 주문 수량
+
+//    protected OrderItem() {
+//
+//    }
+
+    //== 생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        // 재고 수량을 원복해준다.
+        getItem().addStock(count);
+
+    }
+
+    //== 주문 전체 가격 조회 ==//
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
