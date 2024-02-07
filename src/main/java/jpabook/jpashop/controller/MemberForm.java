963133229b61b5f69f6a366259defc3edaeeab8e
yerimya <syl1602@naver.com>
2024-02-07 21:46:52 +0900

    feat: 회원 등록

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotNull(message = "회원 이름은 필수 입니다")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
